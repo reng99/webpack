308ebb2268761fee16c23ad49deac393726a4b3e
reng <1837895991@qq.com>
2017-07-25 14:43:11 +0800

    改为本地开发localhost

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -10,7 +10,8 @@ module.exports = merge(baseWebpackConfig,{
         contentBase:path.join(__dirname,'dist'),// 告诉服务器从哪来提供内容。只有在你想要提供静态文件时才需要。
         publicPath:'/',//用于确定从哪里提供bundle，并且此选项优先
         compress:true,//一切服务都启用「gzip」压缩
-        host:"172.16.1.91",//默认是一个localhost.如果希望外部服务器可以访问，指点成自己的电脑的ip地址，https://doc.webpack-china.org/configuration/dev-server/#devserver-host-cli-
+        host:"localhost",
+        //host:"172.16.1.91",//默认是一个localhost.如果希望外部服务器可以访问，指点成自己的电脑的ip地址，https://doc.webpack-china.org/configuration/dev-server/#devserver-host-cli-
         //quiet:true,//除了初始化启动信息之外的任何内容都不会被打印到控制台。这也就意味着来自webpack的错误或警告在控制台不可见
     },
     plugins:[
